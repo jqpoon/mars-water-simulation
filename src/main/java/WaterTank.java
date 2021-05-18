c97371fd739292860dc163c3a4681400f594c5d3
Jia Qi Poon <poonjiaqi@gmail.com>
2021-05-19 02:56:44 +0800

    Fix bug where water level not reduced when withdrawing

--- src/main/java/WaterTank.java
+++ src/main/java/WaterTank.java
@@ -2,12 +2,19 @@ public class WaterTank {
 
   private float currentVolume = 0;
 
+  public float getCurrentVolume() {
+    return currentVolume;
+  }
+
   /* Returns actual volume of water withdrawn. */
   public float withdrawWater(float volume) {
-    if (currentVolume < volume) {
-      return volume - currentVolume;
+    if (volume >= currentVolume) {
+      float actualVolumeReturned = currentVolume;
+      currentVolume = 0;
+      return actualVolumeReturned;
     }
 
+    currentVolume -= volume;
     return volume;
   }
 
--- src/test/java/WaterTankTest.java
+++ src/test/java/WaterTankTest.java
@@ -5,9 +5,26 @@ import org.junit.jupiter.api.Test;
 public class WaterTankTest {
 
   @Test
+  public void canDepositToWaterTank() {
+    WaterTank tank = new WaterTank();
+    tank.depositWater(10);
+    assertEquals(10, tank.getCurrentVolume());
+  }
+
+  @Test
   public void canWithdrawFromWaterTank() {
     WaterTank tank = new WaterTank();
     tank.depositWater(10);
-    assertEquals(tank.withdrawWater(5), 5);
+    assertEquals(10, tank.withdrawWater(10));
+    assertEquals(0, tank.getCurrentVolume());
   }
+
+  @Test
+  public void cannotWithdrawWhenEmpty() {
+    WaterTank tank = new WaterTank();
+    assertEquals(0, tank.withdrawWater(10));
+    assertEquals(0, tank.getCurrentVolume());
+  }
+
+
 }
