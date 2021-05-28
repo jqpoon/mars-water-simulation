23aa3d63dedc977d93c03c2d43b3c782d08b288f
Jia Qi Poon <jqp18@ic.ac.uk>
2021-05-28 23:50:04 +0800

    Update CentralSystemSim to include most, if not all entities

--- src/main/java/simulation/central/CentralSystemSim.java
+++ src/main/java/simulation/central/CentralSystemSim.java
@@ -5,6 +5,7 @@ import entities.HumanBuilder;
 import entities.WaterConverter;
 import entities.WaterConverterBuilder;
 import entities.WaterGenerator;
+import entities.WaterUseCase;
 import entities.tanks.LowQualityWaterTank;
 import entities.tanks.SmartWaterTank;
 import entities.tanks.WaterTank;
@@ -17,45 +18,77 @@ public class CentralSystemSim extends Simulation<CentralSystemSim> {
 
   private static final int HOURS_IN_A_DAY = 24;
 
-  private final int simulationDays;
-  private final int population;
+  /* Simulation parameters. */
+  private static final int POPULATION = 1;
+  private static final int NO_OF_SIMULATION_DAYS = 1;
+
+  private static final double VOLUME_PER_GENERATION = 4.128;
+  private static final double CONVERTER_EFFICIENCY = 0.935;
+  private static final double VOLUME_PER_CONVERSION = 10000;
+
+  /* Volumes listed are per day per person. */
+  private static final double DAILY_MAXIMUM_VOLUME_USABLE = 10;
+  public static final double DRINK_VOLUME = 1.62;
+  public static final double CROP_VOLUME = 1.34;
+  public static final double HYGIENE_VOLUME = 0.428;
+  public static final double LAUNDRY_VOLUME = 7.33;
+  public static final double FLUSH_VOLUME = 0.067;
+  public static final double ELECTROLYSIS_VOLUME = 2.663;
+
+  /* Frequency of events. If not listed, this event is scheduled
+   * once a day. */
+  public static final int DRINK_FREQUENCY = 10;
+  public static final int HYGIENE_FREQUENCY = 2;
 
   private final Map<Integer, Human> allHumans = new HashMap<>();
-  private final WaterTank centralWaterTank;
+  private final SmartWaterTank centralWaterTank;
   private final WaterTank wasteWaterTank;
   private final WaterGenerator productionUnit;
   private final WaterConverter waterRecycler;
   private final Random randomInst;
 
-  public CentralSystemSim(int simulationDays, int population,
-      double maximumDailyVolume,
-      double volumePerGeneration,
-      double converterEfficiency, double volumePerConversion) {
+  public CentralSystemSim(double drinkingPercentage, double cropPercentage,
+      double hygienePercentage, double laundryPercentage,
+      double flushPercentage, double medicalPercentage,
+      double electrolysisPercentage) {
 
     /* Initialise simulation related fields. */
-    this.simulationDays = simulationDays;
-    this.population = population;
     this.randomInst = new Random(42);
 
     /* Initialise tank entities. */
-    centralWaterTank = new SmartWaterTank(maximumDailyVolume);
+    centralWaterTank = new SmartWaterTank(DAILY_MAXIMUM_VOLUME_USABLE);
     wasteWaterTank = new LowQualityWaterTank();
-    productionUnit = new WaterGenerator(centralWaterTank, volumePerGeneration);
+    productionUnit = new WaterGenerator(centralWaterTank,
+        VOLUME_PER_GENERATION);
     waterRecycler = new WaterConverterBuilder()
-        .withEfficiency(converterEfficiency)
+        .withEfficiency(CONVERTER_EFFICIENCY)
         .withSourceTank(wasteWaterTank)
         .withDestinationTank(centralWaterTank)
-        .withVolumePerConversion(volumePerConversion)
+        .withVolumePerConversion(VOLUME_PER_CONVERSION)
         .build();
 
     /* Create humans. */
-    for (int i = 0; i < population; i++) {
+    for (int i = 0; i < POPULATION; i++) {
       Human human = new HumanBuilder()
           .withPotableWaterTank(centralWaterTank)
           .withWasteWaterTank(wasteWaterTank)
           .build();
       allHumans.put(i, human);
     }
+
+    /* Add ratios for water allocation. */
+    centralWaterTank.addUseCase(WaterUseCase.DRINK, drinkingPercentage);
+    centralWaterTank.addUseCase(WaterUseCase.CROP, cropPercentage);
+    centralWaterTank.addUseCase(WaterUseCase.HYGIENE, hygienePercentage);
+    centralWaterTank.addUseCase(WaterUseCase.LAUNDRY, laundryPercentage);
+    centralWaterTank.addUseCase(WaterUseCase.FLUSH, flushPercentage);
+    centralWaterTank.addUseCase(WaterUseCase.MEDICAL, medicalPercentage);
+    centralWaterTank.addUseCase(WaterUseCase.ELECTROLYSIS,
+        electrolysisPercentage);
+  }
+
+  public Random getRandomInst() {
+    return randomInst;
   }
 
   public WaterGenerator getProductionUnit() {
@@ -73,7 +106,7 @@ public class CentralSystemSim extends Simulation<CentralSystemSim> {
   @Override
   protected boolean stop() {
     /* Simulation only stops after a certain number of days */
-    return getCurrentTime() > HOURS_IN_A_DAY * simulationDays;
+    return getCurrentTime() > HOURS_IN_A_DAY * NO_OF_SIMULATION_DAYS;
   }
 
   @Override
@@ -85,4 +118,9 @@ public class CentralSystemSim extends Simulation<CentralSystemSim> {
   protected void initSimulation() {
 
   }
+
+  public static void main(String[] args) {
+
+  }
+
 }
